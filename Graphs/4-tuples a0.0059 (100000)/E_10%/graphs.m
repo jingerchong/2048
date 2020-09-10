dddf4de8c0ff1a1db4fab61a4b630b139939e187
Jinger Chong <jinge@Cooper.mit.edu>
2020-09-10 17:18:16 +0800

    09/10 5:15PM

--- Graphs/4-tuples a0.0059 (100000)/E_10%/graphs.m
+++ Graphs/4-tuples a0.0059 (100000)/E_10%/graphs.m
@@ -1,4 +1,4 @@
-FileNames = ['A_10%.txt'
+FileNames1 = ['A_10%.txt'
              'A_20%.txt'
              'A_30%.txt'
              'A_40%.txt'
@@ -7,26 +7,46 @@ FileNames = ['A_10%.txt'
              'A_70%.txt'
              'A_80%.txt'
              'A_90%.txt'];
+         
+FileNames2 = ['E_10%.txt'
+             'E_20%.txt'
+             'E_30%.txt'
+             'E_40%.txt'
+             'E_50%.txt'
+             'E_60%.txt'
+             'E_70%.txt'
+             'E_80%.txt'
+             'E_90%.txt'];
 
-ave = zeros(9,100);
-max = zeros(9,100);
-win = zeros(9,100);
+
+ave = zeros(9,200);
+max = zeros(9,200);
+win = zeros(9,200);
 
 for i = (1:9)
-    FileName = FileNames(i,:);
+    FileName = FileNames1(i,:);
     all  = strsplit(fileread(FileName), '\n');
     table = reshape(all,[8,100]);
     ave_ = str2double(erase(table(2,:),'Ave score: '));
     max_ = str2double(erase(table(3,:),'Max score: '));
     win_ = str2double(erase(table(7,:),'Win rate: '));
-    ave(i,:) = ave_;
-    max(i,:) = max_;
-    win(i,:) = win_;
+    ave(i,101:200) = ave_;
+    max(i,101:200) = max_;
+    win(i,101:200) = win_;
+    FileName = FileNames2(i,:);
+    all  = strsplit(fileread(FileName), '\n');
+    table = reshape(all,[8,100]);
+    ave_ = str2double(erase(table(2,:),'Ave score: '));
+    max_ = str2double(erase(table(3,:),'Max score: '));
+    win_ = str2double(erase(table(7,:),'Win rate: '));
+    ave(i,1:100) = ave_;
+    max(i,1:100) = max_;
+    win(i,1:100) = win_;
 end
 
-change = 50000;
+change = 100000;
 
-rounds = (1:100)*1000;
+rounds = (1:200)*1000;
 legends = ['A_1_0'; 'A_2_0'; 'A_3_0'; 'A_4_0'; 'A_5_0'; 'A_6_0'; 'A_7_0'; 'A_8_0'; 'A_9_0'];
 
 f1= figure('Name','Ave score');
